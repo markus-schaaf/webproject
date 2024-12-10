7ebe54668d7089e5ad9d95ba4436b9d11e849f01
T95Forst <thomas.forst95@gmail.com>
2024-12-10 13:35:03 +0100

    Update models.py

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class User (models.Model):
-  user_id = models.IntegerField(primary_key=True)
-  user_first_name = models.CharField(max_length=30)
-  user_last_name = models.CharField(max_length=30)
-  created_at = models.DateTimeField(auto_now_add=True)
-  weight = models.DecimalField(max_digits=4, decimal_places=1)
-  weight_target = models.DecimalField(max_digits=4, decimal_places=1)
+  user_id = models.IntegerField (primary_key=True)
+  user_first_name = models.CharField (max_length=30)
+  user_last_name = models.CharField (max_length=30)
+  created_at = models.DateTimeField (auto_now_add=True)
+  weight = models.DecimalField (max_digits=4, decimal_places=1)
+  weight_target = models.DecimalField (max_digits=4, decimal_places=1)
   daily_calorie_target = models.IntegerField
 
   def __str__(self):
@@ -17,17 +17,28 @@ class User (models.Model):
 
 class Workout_Type (models.Model):
   workout_type_id = models.IntegerField(primary_key=True)
-  workout_name = models.CharField(max_length=80)
-  workout_category = models.CharField(max_length=30)
-  calories_per_kg_per_h = models.DecimalField(max_digits=16, decimal_places=15)
+  workout_name = models.CharField (max_length=80)
+  workout_category = models.CharField (max_length=30)
+  calories_per_kg_per_h = models.DecimalField (max_digits=16, decimal_places=15)
 
   def __str__(self):
     return self.workout_type_id
 
 
 class Workout (models.Model):
-  workout_id = models.IntegerField(primary_key=True)
-  user_id = models.ForeignKey(User.user_id, on_delete=models.CASCADE)
+  workout_id = models.IntegerField (primary_key=True)
+  user_id = models.ForeignKey (User.user_id, on_delete=models.CASCADE)
   time = models.DateTimeField
-  workout_type_id = models.IntegerField          
-  
+  workout_type_id = models.IntegerField
+  workout_length = models.TimeField
+  calories_per_kg_per_h = models.DecimalField (max_digits=16, decimal_places=15)
+  weight = models.DecimalField (max_digits=4, decimal_places=1)
+  calories_burned = models.IntegerField
+
+  def __str__(self):
+    return self.workout_id
+
+
+class Daily_Food (models.Model):
+  daily_food_id = models.IntegerField (primary_key=True)
+  day = models.DateField 
